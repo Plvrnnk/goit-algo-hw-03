0c8e91dd1019e97d3344c773e16b15cdd5605775
Polina Lavrynenko <125290836+Plvrnnk@users.noreply.github.com>
2024-11-29 19:19:10 +0200

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+def get_days_from_today(date): # calculates the number of days between a given date and the current date.
+    now = datetime.today()
+    date = datetime.strptime(date, '%Y-%m-%d')
+    difference = now - date
+    if difference.days < 0: # if given date is in the future, change the calculation sequence. 
+        difference = date - now
+    return difference
+
+text = get_days_from_today("2025-10-09")
+print(text)
---
+++ code2.py
@@ -0,0 +1,28 @@
+import random
+
+def quantity_check(quantity, min_val, max_val): # creating an additional function that checks the correctness of the quantity entered.
+    while quantity < 2 or quantity > (max_val - min_val + 1): # setting while-clause
+        print('Quantity must be at least 2 and fit within the range size.')
+        try:
+            quantity = int(input("Enter quantity: "))
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+    return quantity
+
+def get_numbers_ticket(min_val, max_val, quantity):
+    ticket_list = [] # creating our ticket-list
+    while min_val < 1 or max_val > 1000 or max_val - min_val + 1 < quantity: # setting while-clause
+        try:
+            min_val = int(input("Enter min (>= 1):"))
+            max_val = int(input("Enter max (<= 1000):"))
+            quantity = quantity_check(quantity, min_val, max_val)
+        except ValueError:
+            print("Invalid input. Please enter valid integers.")
+    while len(ticket_list) < quantity: # checking if the length of the list does not exceed the required quantity
+        new_number = random.randint(min_val,max_val) #creating our values for ticket
+        ticket_list.append(new_number) # inserting values
+        ticket_list = sorted(ticket_list) # sorting the list
+    return ticket_list
+
+ticket = get_numbers_ticket(1, 5000, 5)
+print("Generated ticket:", ticket)
