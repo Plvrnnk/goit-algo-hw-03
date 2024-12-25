b4ff6d515e0e23faea3c0749c5409c19bb4146bc
Polina Lavrynenko <125290836+Plvrnnk@users.noreply.github.com>
2024-12-25 22:15:35 +0200

    code2.py

--- code2.py
+++ code2.py
@@ -6,11 +6,13 @@ def get_numbers_ticket(min, max, quantity):
         return ticket_list
     if max > 1000:
         return ticket_list
+    if (max - min + 1) < quantity:
+        return ticket_list
     while len(ticket_list) < quantity:
         new_number = random.randint(min, max)
         if new_number not in ticket_list: 
             ticket_list.append(new_number)
     return sorted(ticket_list)
     
-ticket = get_numbers_ticket(1, 1000, 5)
+ticket = get_numbers_ticket(10,14,6)
 print("Generated ticket:", ticket)
