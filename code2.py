550dd3b88ec1b68b540df391f492fc36bfabd400
Polina Lavrynenko <125290836+Plvrnnk@users.noreply.github.com>
2024-12-25 17:02:45 +0200

    Update code2.py

--- code2.py
+++ code2.py
@@ -1,28 +1,16 @@
 import random
 
-def quantity_check(quantity, min_val, max_val): # creating an additional function that checks the correctness of the quantity entered.
-    while quantity < 2 or quantity > (max_val - min_val + 1): # setting while-clause
-        print('Quantity must be at least 2 and fit within the range size.')
-        try:
-            quantity = int(input("Enter quantity: "))
-        except ValueError:
-            print("Invalid input. Please enter a valid integer.")
-    return quantity
-
-def get_numbers_ticket(min_val, max_val, quantity):
-    ticket_list = [] # creating our ticket-list
-    while min_val < 1 or max_val > 1000 or max_val - min_val + 1 < quantity: # setting while-clause
-        try:
-            min_val = int(input("Enter min (>= 1):"))
-            max_val = int(input("Enter max (<= 1000):"))
-            quantity = quantity_check(quantity, min_val, max_val)
-        except ValueError:
-            print("Invalid input. Please enter valid integers.")
-    while len(ticket_list) < quantity: # checking if the length of the list does not exceed the required quantity
-        new_number = random.randint(min_val,max_val) #creating our values for ticket
-        ticket_list.append(new_number) # inserting values
-        ticket_list = sorted(ticket_list) # sorting the list
-    return ticket_list
-
-ticket = get_numbers_ticket(1, 50, 5)
+def get_numbers_ticket(min, max, quantity):
+    ticket_list = []
+    if min < 1:
+        return ticket_list
+    if max > 1000:
+        return ticket_list
+    while len(ticket_list) < quantity:
+        new_number = random.randint(min, max)
+        if new_number not in ticket_list: 
+            ticket_list.append(new_number)
+    return sorted(ticket_list)
+    
+ticket = get_numbers_ticket(1, 1000, 5)
 print("Generated ticket:", ticket)
