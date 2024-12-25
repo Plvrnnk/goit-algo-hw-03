090854452f2a962e23c6e902aeeca648695ead51
Polina Lavrynenko <125290836+Plvrnnk@users.noreply.github.com>
2024-12-25 16:48:30 +0200

    Update code1.py

--- code1.py
+++ code1.py
@@ -6,7 +6,7 @@ def get_days_from_today(date): # calculates the number of days between a given d
     difference = now - date
     if difference.days < 0: # if given date is in the future, change the calculation sequence. 
         difference = date - now
-    return difference
+    return int(difference.days)
 
 text = get_days_from_today("2025-10-09")
 print(text)
